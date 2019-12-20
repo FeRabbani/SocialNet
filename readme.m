a05a07b9177c40792993aa64a504e3d233b32fe9
fereshteh <fereshte.rabbani@gmail.com>
2019-12-20 14:26:27 +0100

    Paper

---
+++ readme.m
@@ -0,0 +1,6 @@
+Here, We use network theoretical approaches to analyze the data we obtained from web forums in public and darkweb by studying the
+connectivity features of the members and the threads within a wide selection of forums
+(including dark and semi-dark) and establish several characteristic behavioral patterns.
+Our findings reveal both common and rather different features in the two types of
+behavior.
+
