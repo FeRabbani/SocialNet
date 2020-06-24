0b1f06629f679281b0fba3786c2004d956701bba
Fereshteh Rabbani <53039857+FeRabbani@users.noreply.github.com>
2020-06-24 19:24:15 +0200

    Update and rename readme.m to readme.md

--- readme.m
+++
@@ -1,4 +0,0 @@
-# Analyzing online user's behavior
-I have used network theoretical approaches to analyze the unstructured data obtained across different platforms and establish several characteristic behavioral patterns.
-
-
